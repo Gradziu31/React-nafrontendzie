3d2a53f70d2f269c0a45e01eead2ac6c04f1bc87
NoweTestoweKonto <k.gradek31@gmail.com>
2018-08-29 00:25:45 +0200

    form support on the example of select

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import events from './data/events.json';
-import Component from './Events.js';
+// import events from './data/events.json';
+// import Component from './Events.js';
 
-ReactDOM.render(<Component events={events}/>, document.getElementById('root'));
+class Component extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { car: 'audi' };
+    }
+   
+    onOptionChange(event) {
+      const newValue = event.currentTarget.value;
+      this.setState({ car: newValue });
+    }
+   
+    render() {
+      return (
+        <form>
+          <select onChange={this.onOptionChange.bind(this)} value={this.state.car}>
+            <option value="fiat">Fiat</option>
+            <option value="audi">Audi</option>
+            <option value="mercedes">Mercedes</option>
+          </select>
+        </form>
+      );
+    }
+  }
+
+ReactDOM.render(<Component />, document.getElementById('root'));
+// ReactDOM.render(<Component events={events}/>, document.getElementById('root'));
