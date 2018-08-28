51acf52aaf95090a4a12efd328ed39e91864d348
NoweTestoweKonto <k.gradek31@gmail.com>
2018-08-28 21:59:21 +0200

    Property children's props & PropTypes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import events from './data/events.json';
-import Component from './Events.js';
+import PropTypes from 'prop-types';
+// import Component from './Events.js';
 
-// const Component = (props) => {
-//     return <p>{props.text}</p>
-// } 
+// dobra opcja do opakowania kodu, wewnątrz mogę umieścić np. inny komponent (przykład użycia <ShowChildren>Hello! World!</ShowChildren>, wystarczy opleść nim kod)
+const ShowChildren = (props) => {
+    return (
+        <div>
+        <h1>{props.children}</h1>
+        </div>
+    );
+}
 
-ReactDOM.render(<Component events={events} />, document.getElementById('root'));
+// Kontrola typów przekazywanych do komponentu
+// poniżej ustawiłem np. za pomocą prop-types że text ma przyjmować string (jeśli warunek nie będzie spełniony, dostaniemy informację w konsoli)
+class HelloComponent extends React.Component {
+    static propTypes = {
+      text: PropTypes.string.isRequired
+    };
+   
+    render() {
+      return <p>{this.props.text}</p>;
+    }
+  }
+
+
+  // W komponencie funkcyjnym wyglądało by to tak:
+//   const HelloComponent2 = (props) => {
+//     return <p>{props.text}</p>;
+//   };
+   
+//   HelloComponent.propTypes = {
+//     text: PropTypes.string.isRequired
+//   };
+  
+ReactDOM.render(<HelloComponent text='Lorem ipsum...' />, document.getElementById('root'));
