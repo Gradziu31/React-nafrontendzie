f9875856f06f09d86e272fca50852d10214c0552
NoweTestoweKonto <k.gradek31@gmail.com>
2018-08-28 22:28:14 +0200

    - initialization of the initial state - forwarding status to nested components - component status update

--- src/index.js
+++ src/index.js
@@ -1,22 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import events from './data/events.json';
-import PropTypes from 'prop-types';
-import Component from './Events.js';
+// import events from './data/events.json';
+// import PropTypes from 'prop-types';
+// import Component from './Events.js';
 
 
-// Kontrola typów przekazywanych do komponentu
-// poniżej ustawiłem np. za pomocą prop-types że text ma przyjmować string (jeśli warunek nie będzie spełniony, dostaniemy informację w konsoli)
-class HelloComponent extends React.Component {
-    static propTypes = {
-      text: PropTypes.string.isRequired
-    };
-   
-    render() {
-      return <p>{this.props.text}</p>;
+// Poniżej przykład w którym przekazuję stan początkowy do zagnieżdzonego komponentu
+class HomeComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            text: 'Janusz', 
+            age: 57,
+            click: 0,
+        };
     }
-  }
+    render(){
+        return (
+            <div>
+            <h1>Wyrenderowane ze stanu:</h1>
+            <p>Imię: {this.state.text}</p>
+            <p>Wiek: {this.state.age}</p>
+            <UserData text={this.state.text} age={this.state.age}/>
+            <div>
+                <h1>Hit Click: {this.state.click}</h1>
+                <button onClick={() => this.setState({click: this.state.click + 1})}>HIT!</button>
+            </div>
+            </div>
+        )
+    }   
+}
 
+const UserData = (props) => {
+    return (
+      <div>
+      <h1>Przekazane wartości przez props:</h1>
+        <p>Imię: {props.text}</p>
+        <p><strong>wiek:</strong> {props.age}</p>
+      </div>
+    );
+}
 
-  
-ReactDOM.render(<Component events={events}/>, document.getElementById('root'));
+// ReactDOM.render(<Component events={events}/>, document.getElementById('root'));
+ReactDOM.render(<HomeComponent />, document.getElementById('root'));
